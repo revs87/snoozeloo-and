11db4fda63c3f0871e52b91b9595b9939ed72c4b
Rui Vieira <revs87@msn.com>
2024-11-28 20:15:11 +0000

    NameCard focus release upon keyboard closing

--- app/src/main/java/com/rvcoding/snoozeloo/ui/component/NameCard.kt
+++ app/src/main/java/com/rvcoding/snoozeloo/ui/component/NameCard.kt
@@ -3,6 +3,8 @@ package com.rvcoding.snoozeloo.ui.component
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.LocalTextStyle
@@ -15,8 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rvcoding.snoozeloo.R
@@ -36,6 +40,7 @@ fun NameCard(
     onAction: (Actions.AlarmSettings) -> Unit
 ) {
     var name by remember { mutableStateOf(state.alarm.name) }
+    val focusManager = LocalFocusManager.current
 
     Card(
         modifier = Modifier
@@ -79,6 +84,12 @@ fun NameCard(
                     unfocusedPlaceholderColor = if (isDarkTheme()) TextSecondaryDark else TextSecondary,
                     focusedIndicatorColor = if (isDarkTheme()) PrimaryDark else Primary,
                     unfocusedIndicatorColor = if (isDarkTheme()) TextSecondaryDark else TextSecondary,
+                ),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                    }
                 )
             )
         }
